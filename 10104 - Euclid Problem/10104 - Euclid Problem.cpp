c683304570e23d98e2feeea59289261113c1d1b6
Artcs <ju.jeffri.v@gmail.com>
2019-02-03 19:55:25 -0200

    10104

---
+++ 10104 - Euclid Problem/10104 - Euclid Problem.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int x,y,d;
+
+void extendedEuclid(int a,int b)
+{
+    if(b==0){ x=1;y=0;d=a;return; }
+    extendedEuclid(b,a%b);
+    int x1 = y;
+    int y1 = x - (a/b)*y;
+    x = x1;
+    y = y1;
+}
+
+int main()
+{
+    int a,b;
+    while(~scanf("%d %d",&a,&b))
+    {
+        extendedEuclid(a,b);
+        printf("%d %d %d\n",x,y,d);
+    }
+    return 0;
+}
